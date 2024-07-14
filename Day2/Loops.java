6934b2767f6b82e7113f87066d212b3d0b94a265
Ankit Shokeen <ankitshokeen2001@gmail.com>
2024-07-14 12:20:19 +0530

    Day2 commit

---
+++ Day2/Loops.java
@@ -0,0 +1,35 @@
+public class Loops {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        //int i;
+
+        //while loop
+        //while (i <= 10) {
+          //  System.out.println(i);
+        //}
+
+        //do while loop
+        //do {
+          //  System.out.println(i);
+        //} while (i <= 10);
+
+       // int n = sc.nextInt();
+        //for loop
+        //for (i=0; i<=n; i++) {
+          //  System.out.print("* ");
+       // }
+       // System.out.println();
+       // for (i=0; i<=n; i++) {
+         //   System.out.print("i ");
+       // }
+
+        //nested for loop
+        for (int i=1; i<=5; i++) {
+            for (int j=1; j<=5; j++) {
+                System.out.print("1 ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Day2/PatternQuestions.java
@@ -0,0 +1,44 @@
+public class PatternQuestions {
+    public static void main(String[] args) {
+
+//        Q1
+//        for (int i=1; i<=5; i++) {
+//            for (int j=1; j<=5; j++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//
+//        Q2
+//        for (int i=1; i<=5; i++) {
+//            for (int j=1; j<=5; j++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//
+//        Q3
+//         for (int i=5; i>=1; i--) {
+//             for (int j=1; j<=i; j++) {
+//                 System.out.print("* ");
+//             }
+//             System.out.println();
+//         }
+//
+//         Q4
+//        for (int i=1; i<=5; i++) {
+//            for (int j=1; j<=5; j++) {
+//                if (i==1 || j==1) {
+//                    System.out.print("* ");
+//                } else if (i==5 || j==5) {
+//                    System.out.print("* ");
+//                } else {
+//                    System.out.print("  ");
+//                }
+//            }
+//            System.out.println();
+//        }
+
+
+    }
+}
