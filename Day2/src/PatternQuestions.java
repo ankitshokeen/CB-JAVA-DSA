4ebde02c233de35ece6a3a6301e78c10314402b5
ankitshokeen <ankitshokeen2001@gmail.com>
2024-07-17 01:52:33 +0530

    Day2 commit

--- Day2/src/PatternQuestions.java
+++ Day2/src/PatternQuestions.java
@@ -38,7 +38,47 @@ public class PatternQuestions {
 //            }
 //            System.out.println();
 //        }
+//
+//        Q5
+//        for (int i=1; i<=n*2; i++) {
+//            if (i <= n) {
+//                for (int j=1; j<=i; j++) {
+//                    System.out.print("* ");
+//                }
+//            } else {
+//                for (int k=n-1; k>=i-n; k--) {
+//                    System.out.print("* ");
+//                }
+//            }
+//            System.out.println();
+//        }
+//
+//        Q6
+//         for (int i=1; i<=5; i++) {
+//             for (int j=1; j<=i; j++) {
+//                 System.out.print(j);
+//             }
+//             System.out.println();
+//         }
+//
+//        Q7
+//         for (int i=5; i>=1; i--) {
+//             for (int j=1; j<=i; j++) {
+//                 System.out.print(j);
+//             }
+//             System.out.println();
+//         }
 
-
+        //Q8
+        for (int i=1; i<=5; i++) {
+            for (int j=1; j<=5; j++) {
+                if (i == j || i + j == 6) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
